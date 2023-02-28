04ce847ccacfbab135f5016ad66ab864c4b512ca
Fededada23 <dadamofederico@gmail.com>
2023-02-28 18:42:57 +0100

    fixes

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
-use App\Models\Post;
+use App\Models\Project;
 use App\Http\Controllers\Controller; 
 
 class ProjectController extends Controller
